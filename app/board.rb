9874b2f054260602cbe609e08d5832dd50e1cdfd
Sasha Grodzins <6587024+sassygrody@users.noreply.github.com>
2022-02-27 14:50:15 -0600

    remove unused func

--- app/board.rb
+++ app/board.rb
@@ -15,23 +15,11 @@ class Board
   end
 
   def add_board_buffer
-    # return unless live_cell_is_near_edge
-
     top_row_needs_buffer? && grid.unshift(Array.new(grid.length, "-"))
     bottom_row_needs_buffer? && grid.push(Array.new(grid.length, "-"))
     left_side_needs_buffer? && grid.each { |row| row.unshift("-") }
     right_side_needs_buffer? && grid.each { |row| row.push("-") }
   end
-
-  # def live_cell_is_near_edge
-  #   # prevent buffer from adding if new live cell isn't within on edge
-  #   return if grid.empty?
-
-  #   top_row_needs_buffer ||
-  #     bottom_row_needs_buffer ||
-  #     left_side_needs_buffer ||
-  #     right_side_needs_buffer
-  # end
 
   def top_row_needs_buffer?
     grid.first.any? { |cell| cell == "+" }
