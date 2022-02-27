407f6cb973728e24d2e3d8a28702e4b3dc5c7e55
Sasha Grodzins <6587024+sassygrody@users.noreply.github.com>
2022-02-27 11:04:18 -0600

    wow what a mess--keeping track of the current cell neighbord states

--- app/game.rb
+++ app/game.rb
@@ -13,7 +13,10 @@ class Game
     puts "......GAME OF LIFE.....kinda"
     puts "............................"
     puts
-    set_seed_cells_on_board
+    put_seed_cells_in_board!
+    display_board
+    sleep 2
+    update_live_cells_in_board!
     display_board
   end
 
@@ -27,16 +30,56 @@ class Game
   end
 
   def display_board
+    puts # space between new board output
     @board.map do |cell|
       puts cell.join(" ")
     end
   end
 
-  def set_seed_cells_on_board
+  def put_seed_cells_in_board!
     return if seed_cells.empty?
 
-    @seed_cells.each do |cell|
-      @board[cell[:row]][cell[:col]] = "+"
+    seed_cells.each do |cell|
+      board[cell[:row]][cell[:col]] = "+"
+    end
+  end
+
+  def update_live_cells_in_board!
+    return if seed_cells.empty?
+
+    seed_cells.each do |cell|
+      left_top = board[cell[:row] - 1][cell[:col] - 1]
+      center_top = board[cell[:row] - 1][cell[:col]]
+      right_top = board[cell[:row] - 1][cell[:col] + 1]
+      left_bottom = board[cell[:row] + 1][cell[:col] - 1]
+      center_bottom = board[cell[:row] + 1][cell[:col]]
+      right_bottom = board[cell[:row] + 1][cell[:col] + 1]
+      left_size = board[cell[:row]][cell[:col] - 1]
+      right_size = board[cell[:row]][cell[:col] + 1]
+      current_cell = board[cell[:row][cell[:col]]]
+      neighbors = [left_top,
+                   center_top,
+                   right_top,
+                   left_bottom,
+                   center_bottom,
+                   right_bottom,
+                   left_size,
+                   right_size]
+      live_neighbors = neighbors - %w[-]
+
+      if current_cell == "+"
+        # apply live rules
+
+        if live_neighbors.count == 2 || live_neighbors.count == 3
+          # keep cell coords in cell list
+        else
+          # kill cell because it's lonely or overpopulated
+        end
+      else
+        # apply dead rules
+        live_neighbors.count == 3 # &&  activate cell
+        end
+      end
     end
   end
 
