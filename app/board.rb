0fda733dd8306bf7533c67fe6b5ffaca567558a2
Sasha Grodzins <6587024+sassygrody@users.noreply.github.com>
2022-02-27 14:49:56 -0600

    cleaning up buffer logic, but it is not right yet

--- app/board.rb
+++ app/board.rb
@@ -6,7 +6,7 @@ class Board
   attr_accessor :grid
 
   def display_board
-    system "clear"
+    # system "clear"
     puts # space between new board output
     add_board_buffer
     @grid.map do |cell|
@@ -15,30 +15,37 @@ class Board
   end
 
   def add_board_buffer
-    return unless live_cell_is_near_edge
+    # return unless live_cell_is_near_edge
 
-    top_row_padding = Array.new(grid.length, "-")
-    bottom_row_padding = Array.new(grid.length, "-")
-    grid.unshift(top_row_padding)
-    grid.push(bottom_row_padding)
-    grid.each do |row|
-      row.unshift("-")
-      row.push("-")
-    end
+    top_row_needs_buffer? && grid.unshift(Array.new(grid.length, "-"))
+    bottom_row_needs_buffer? && grid.push(Array.new(grid.length, "-"))
+    left_side_needs_buffer? && grid.each { |row| row.unshift("-") }
+    right_side_needs_buffer? && grid.each { |row| row.push("-") }
   end
 
-  def live_cell_is_near_edge
-    # prevent buffer from adding if new live cell isn't within on edge
-    return if grid.empty?
+  # def live_cell_is_near_edge
+  #   # prevent buffer from adding if new live cell isn't within on edge
+  #   return if grid.empty?
 
-    top_row_needs_buffer = grid.first.any? { |cell| cell == "+" }
-    bottom_row_needs_buffer = grid.last.any? { |cell| cell == "+" }
-    left_side_needs_button = grid.any? { |row| row[0] == "+" }
-    right_side_needs_button = grid.any? { |row| row[-1] == "+" }
+  #   top_row_needs_buffer ||
+  #     bottom_row_needs_buffer ||
+  #     left_side_needs_buffer ||
+  #     right_side_needs_buffer
+  # end
 
-    top_row_needs_buffer ||
-      bottom_row_needs_buffer ||
-      left_side_needs_button ||
-      right_side_needs_button
+  def top_row_needs_buffer?
+    grid.first.any? { |cell| cell == "+" }
+  end
+
+  def bottom_row_needs_buffer?
+    grid.last.any? { |cell| cell == "+" }
+  end
+
+  def left_side_needs_buffer?
+    grid.any? { |row| row[0] == "+" }
+  end
+
+  def right_side_needs_buffer?
+    grid.any? { |row| row[-1] == "+" }
   end
 end
